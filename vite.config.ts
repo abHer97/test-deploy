310609badf42d8764d4a181094900014310aa449
Abel Herrera <ahpard97@gmail.com>
2024-02-10 13:46:16 -0600

    chore: configure tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -5,4 +6,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: './',
   plugins: [react()],
+  test: {
+    globals: true,
+    include: ['src/**/*.{test,spec}.{js,ts,jsx,tsx}'],
+    environment: 'jsdom',
+  },
 });
